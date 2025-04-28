d44aac1052f39b04ea8999da7702a5ee8138f47f
ThePresidentofTheUniverse <njabbott2005@gmail.com>
2025-04-28 07:42:56 -0500

    Initial Commit

---
+++ src/WeatherDriver.java
@@ -0,0 +1,17 @@
+/*----------------------------------------------------
+ * Program Title: Weather Program With API Implementation
+ * Author: Nolan Abbott
+ * Start Date: [28/04/2025]
+ * End Date: [change/change/2025]
+ * Description: This is a program that allows its users to search up what the weather is like in different locations
+ * with the use of zipcodes (and maybe country abbreviations, I'll have to see how far I can get within the time-span of
+ * 1 week.
+ * Usage / Issues: not available
+ *-----------------------------------------------------
+ */
+public class WeatherDriver {
+
+    public static void main(String[] args) {
+
+    }
+}
