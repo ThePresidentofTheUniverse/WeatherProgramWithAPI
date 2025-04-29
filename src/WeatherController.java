f413c342c8f0db981e79eae8e256a61a8737f17a
ThePresidentofTheUniverse <njabbott2005@gmail.com>
2025-04-29 18:58:58 -0500

    Working on application

--- src/WeatherController.java
+++ src/WeatherController.java
@@ -1,2 +1,18 @@
-public class WeatherController {
+import javafx.collections.FXCollections;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WeatherController implements Initializable {
+    // All fields from the form below
+    public Label Country;
+    public ComboBox<String> cmbCountry;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cmbCountry.setItems(FXCollections.observableArrayList("Andorra","Argentina","American Samoa", "Austria", "Australia", "Bangladesh", "Belgium", "Belgium", "Bulgaria", "Brazil", "Canada", "Switzerland"));
+    }
 }
