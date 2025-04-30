f469a4006a4ae04d9a30db40d762ec9ae925f385
ThePresidentofTheUniverse <njabbott2005@gmail.com>
2025-04-29 20:06:40 -0500

    Working on application

--- src/WeatherController.java
+++ src/WeatherController.java
@@ -1,11 +1,15 @@
 import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WeatherController implements Initializable {
     // All fields from the form below
@@ -17,12 +21,53 @@ public class WeatherController implements Initializable {
     public ComboBox<String> cmbTempUnit;
     public Label lblWindSpeed;
     public ComboBox<String> cmbWindSpeed;
+    public Button btnCheckWeather;
 
+    //Adds information to the combo boxes, perfect for controlling user input
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        cmbCountry.setItems(FXCollections.observableArrayList("Andorra","Argentina","American Samoa", "Austria", "Australia", "Bangladesh", "Belgium", "Belgium", "Bulgaria", "Brazil", "Canada", "Switzerland"));
+        cmbCountry.setItems(FXCollections.observableArrayList("Andorra (AD)","Argentina (AR)","American Samoa (AS)", "Austria (AT)", "Australia (AU)", "Bangladesh (BD)", "Belgium (BE)", "Bulgaria (BG)", "Brazil (BR)", "Canada (CA)", "Switzerland (CH)", "United States (US)"));
         cmbTempUnit.setItems(FXCollections.observableArrayList("Celsius", "Fahrenheit", "Kelvin"));
         cmbWindSpeed.setItems(FXCollections.observableArrayList("km/h", "m/s", "mph", "Knots"));
+    }
+
+    //The button that checks the weather.
+    @FXML
+    void checkingWeather(){
+        try{
+            String country = nameRemover(cmbCountry.getValue());
+            String zipCode = txtZipCode.getText().trim();
+            String TempUnit = cmbTempUnit.getValue();
+            String windSpeed = cmbWindSpeed.getValue();
+
+            System.out.println(country);
+            //Validates user input
+            //Validator.validateInput()
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    //Method that removes the non-parenthesis from country string.
+    public static String nameRemover(String input){
+
+        StringBuilder result = new StringBuilder();
+
+        Pattern pattern = Pattern.compile("\\([A-Z]{2}\\)");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            String match = matcher.group();
+            if(match.startsWith("("))  {
+                result.append(match);
+            }
+        }
+        // Deletes the left-over parenthesis
+        result.deleteCharAt(3);
+        result.deleteCharAt(0);
+        String baseChars = result.toString();
+
+        return baseChars;
     }
 
 
