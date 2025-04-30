ef5a3b63b715f17ba89fce9d0eace570acb1098f
ThePresidentofTheUniverse <njabbott2005@gmail.com>
2025-04-29 19:10:28 -0500

    Working on application

--- src/WeatherController.java
+++ src/WeatherController.java
@@ -2,17 +2,28 @@ import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class WeatherController implements Initializable {
     // All fields from the form below
-    public Label Country;
+    public Label lblCountry;
     public ComboBox<String> cmbCountry;
+    public Label lblZipCode;
+    public TextField txtZipCode;
+    public Label lblTempUnit;
+    public ComboBox<String> cmbTempUnit;
+    public Label lblWindSpeed;
+    public ComboBox<String> cmbWindSpeed;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         cmbCountry.setItems(FXCollections.observableArrayList("Andorra","Argentina","American Samoa", "Austria", "Australia", "Bangladesh", "Belgium", "Belgium", "Bulgaria", "Brazil", "Canada", "Switzerland"));
+        cmbTempUnit.setItems(FXCollections.observableArrayList("Celsius", "Fahrenheit", "Kelvin"));
+        cmbWindSpeed.setItems(FXCollections.observableArrayList("km/h", "m/s", "mph", "Knots"));
     }
+
+
 }
